280614e4c7a810c6fbbfc68221c6966a257ba8cb
Dudinha5 <137414096+Dudinha5@users.noreply.github.com>
2023-09-28 10:35:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ function tocaSom (idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas = documento.querySelectorA11('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento - tecla.classList[1];
-    const idAudio = '#som_${instrumento}`; //template string
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
     tecla.onclick = function () {
         tocaSom(idAudio);
